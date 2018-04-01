26276ff6ad6d221855b490285cc9480044ffc6a2
Pascal Andy <pascal@firepress.org>
2018-04-01 10:07:07 -0400

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+## What is this?
+
+In Ghost, there is a section in inject custom code. It’s under the Code injection section.
+
+![pic](/img/2018-04-01_10h04.jpg)
+
---
+++ firepress_klimax_code-head.sh
@@ -0,0 +1,21 @@
+<!-- FirePress 🔥 📰 -->
+<!-- THEME: Firepress_Klimax -->
+<!-- Overide CSS styles  🔥 📰 -->
+
+<style>
+    
+// DESKTOP resolution {}
+    
+// --- logo size {}
+.blog-logo img {
+    height: 70px;
+}
+
+// SMARTPHONE resolution {}
+
+ @media (max-width: 500px){
+.blog-logo img {
+    height: 50px;
+}
+
+</style>
