5546f894765a10a8b60c885caecb6341b0dd8414
Pascal Andy <pascal@firepress.org>
2018-04-09 22:46:05 -0400

    add Simply

---
+++ Firepress_Simply_CI_header.sh
@@ -0,0 +1,22 @@
+<!-- Header CSS overide | by FirePress.org 🔥 📰 --
+<!-- Theme: Firepress_Simply -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-Ghost -->
+
+<!-- FirePress 🔥 📰 -->
+<!-- Overide CSS styles  🔥 📰 -->
+
+<style>
+    
+    .header-logo img {
+    max-height: 140%;
+	}
+
+@font-face{
+    font-family:simply;
+    src:url(https://play-with-ghost.com/demo-barbershop-simply/assets/fonts/simply.eot);
+    src:url(https://play-with-ghost.com/demo-barbershop-simply/assets/fonts/simply.eot#iefix) format("embedded-opentype"),
+        url(https://play-with-ghost.com/demo-barbershop-simply/assets/fonts/simply.ttf) format("truetype"),
+        url(https://play-with-ghost.com/demo-barbershop-simply/assets/fonts/simply.woff) format("woff"),
+        url(https://play-with-ghost.com/demo-barbershop-simply/assets/fonts/simply.svg#simply) format("svg");  
+
+</style>
