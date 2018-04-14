d6d3920bb8ca1efcc3b75f365699ee502570a0ca
Pascal Andy <pascal@firepress.org>
2018-04-13 23:37:10 -0400

    add attila

---
+++ attila_CI_footer.sh
@@ -0,0 +1,11 @@
+<!-- Footer CSS overide | by FirePress.org 🔥 📰 --
+<!-- Theme: attila -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-Ghost -->
+
+<style>
+    
+// DESKTOP resolution {}
+    
+// SMARTPHONE resolution {}
+
+</style>
---
+++ attila_CI_header.sh
@@ -0,0 +1,22 @@
+<!-- Header CSS overide | by FirePress.org 🔥 📰 --
+<!-- Theme: attila -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-Ghost -->
+
+<style>
+
+// DESKTOP resolution {}
+    
+html {
+    font-size: 70%;
+    overflow-x: hidden;
+}
+
+
+// SMARTPHONE resolution {}
+
+@media only screen and (max-width: 640px)
+html {
+    font-size: 58%;
+}
+
+</style>
