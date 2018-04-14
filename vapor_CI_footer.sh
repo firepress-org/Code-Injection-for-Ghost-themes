a2b37dd7eb4cc3f309256c4f51c83596937854e0
Pascal Andy <pascal@firepress.org>
2018-04-14 17:15:50 -0400

    Added vapor

---
+++ vapor_CI_footer.sh
@@ -0,0 +1,11 @@
+<!-- Footer CSS overide | by FirePress.org 🔥 📰 --
+<!-- Theme: vapor -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-Ghost -->
+
+<style>
+    
+// DESKTOP RESOLUTION {}
+    
+// SMARTPHONE RESOLUTION {}
+
+</style>
