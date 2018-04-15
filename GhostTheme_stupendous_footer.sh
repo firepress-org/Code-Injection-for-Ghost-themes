4581d23f6c23103b7ded9e73d38e1a80746ca6ca
Pascal Andy <pascal@firepress.org>
2018-04-15 13:10:49 -0400

    Add stupendous

---
+++ GhostTheme_stupendous_footer.sh
@@ -0,0 +1,11 @@
+<!-- Footer CSS overide | by FirePress.org 🔥 📰 --
+<!-- Theme: stupendous -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-Ghost -->
+
+<style>
+    
+// DESKTOP RESOLUTION {}
+    
+// SMARTPHONE RESOLUTION {}
+
+</style>
---
+++ GhostTheme_stupendous_header.sh
@@ -0,0 +1,14 @@
+<!-- Header CSS overide | by FirePress.org 🔥 📰 --
+<!-- Theme: stupendous -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-Ghost -->
+
+<!-- FirePress 🔥 📰 -->
+<!-- Overide CSS styles  🔥 📰 -->
+
+<style>
+    
+// DESKTOP RESOLUTION {}
+    
+// SMARTPHONE RESOLUTION {}
+
+</style>
