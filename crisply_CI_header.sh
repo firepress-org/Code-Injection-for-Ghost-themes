f51a9256c00965ed00de8e99cd1a8b1a403a1633
Pascal Andy <pascal@firepress.org>
2018-04-15 13:00:19 -0400

    added crisply

---
+++ crisply_CI_header.sh
@@ -0,0 +1,17 @@
+<!-- Header CSS overide | by FirePress.org 🔥 📰 --
+<!-- Theme: crisply -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-Ghost -->
+
+<!-- FirePress 🔥 📰 -->
+<!-- Overide CSS styles  🔥 📰 -->
+
+<style>
+    
+// DESKTOP RESOLUTION {}
+a {
+    color: #B34A30;
+}
+    
+// SMARTPHONE RESOLUTION {}
+
+</style>
