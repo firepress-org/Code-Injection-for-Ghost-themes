2f79817ff52d2007699b270e9ad56a8993e9c7f0
Pascal Andy <pascal@firepress.org>
2018-05-10 11:56:23 -0400

    add paperleaf

---
+++ paperleaf_header.sh
@@ -0,0 +1,19 @@
+<!-- Header CSS overide | by FirePress.org 🔥 📰 -->
+<!-- Theme: paperleaf -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-for-Ghost-themes -->
+
+
+<style>
+
+// --- DESKTOP RESOLUTION --- {}
+
+body {
+    font-size: 110%;
+}
+.py1 {
+    font-size: .9em;
+}
+
+// --- SMARTPHONE RESOLUTION --- {}
+
+</style>
