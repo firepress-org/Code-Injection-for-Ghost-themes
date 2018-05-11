ce8e51e9f542eb68af34051e48395e9f1827aa0f
Pascal Andy <pascal@firepress.org>
2018-05-11 09:00:38 -0400

    update

--- paperleaf_header.sh
+++ paperleaf_header.sh
@@ -3,6 +3,11 @@
 <!-- Find this template at https://github.com/firepress-org/Code-Injection-for-Ghost-themes -->
 
 
+<!-- Header CSS overide | by FirePress.org 🔥 📰 -->
+<!-- Theme: paperleaf -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-for-Ghost-themes -->
+
+
 <style>
 
 // --- DESKTOP RESOLUTION --- {}
@@ -10,12 +15,36 @@
 body {
     font-size: 110%;
 }
+    
 .py1 {
     font-size: .9em;
 }
 
-// --- SMARTPHONE RESOLUTION --- {}
+// Button no underline {}
+.post-content a {
+    text-decoration: none;
+}
 
+img {
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    padding: 5px;
+}
+    
+.blog-logo img {
+    border: 0px;
+}
+    
+li {
+    margin-bottom: 0.4em;
+}
+
+.grid {
+    min-width: none;
+}
+    
+// --- SMARTPHONE RESOLUTION --- {}
+    
 </style>
 
 <!-- Start of Async Drift Code -->
