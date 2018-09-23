0691124977806fd7fbad7fab7abf518f88232250
Pascal Andy <pascal@firepress.org>
2018-09-23 16:55:31 -0400

    remove font / it's not well manage in the theme itself /b

--- FirePress_Simply_header.sh
+++ FirePress_Simply_header.sh
@@ -12,14 +12,6 @@
     margin-bottom: 40px;
 }
 
-@font-face{
-    font-family:simply;
-    src:url(https://play-with-ghost.com/live-demo-firepress-simply-for-barbershops/assets/fonts/simply.eot);
-    src:url(https://play-with-ghost.com/live-demo-firepress-simply-for-barbershops/assets/fonts/simply.eot#iefix) format("embedded-opentype"),
-        url(https://play-with-ghost.com/live-demo-firepress-simply-for-barbershops/assets/fonts/simply.ttf) format("truetype"),
-        url(https://play-with-ghost.com/live-demo-firepress-simply-for-barbershops/assets/fonts/simply.woff) format("woff"),
-        url(https://play-with-ghost.com/live-demo-firepress-simply-for-barbershops/assets/fonts/simply.svg#simply) format("svg");  
-
 // --- SMARTPHONE RESOLUTION --- {}
 
 </style>
