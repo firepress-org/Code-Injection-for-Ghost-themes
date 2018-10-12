e025e5b92591bcc6ce2855c953904794d10d6d78
Pascal Andy <pascal@firepress.org>
2018-10-11 20:12:08 -0400

    Added FirePress_Biron

---
+++ FirePress_Biron_footer.sh
@@ -0,0 +1,12 @@
+<!-- Footer CSS overide | by FirePress.org 🔥 📰 -->
+<!-- Theme: FirePress_Biron -->
+<!-- Find this template at https://github.com/firepress-org/Code-Injection-for-Ghost-themes -->
+
+
+<style>
+
+// --- DESKTOP RESOLUTION --- {}
+
+// --- SMARTPHONE RESOLUTION --- {}
+
+</style>
